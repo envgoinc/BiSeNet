94f20f1e77585b4d4e2bb8a9531080dad6ef9c42
Lucas Reljic-Dumont <56940300+lucasreljic@users.noreply.github.com>
2025-08-22 17:50:37 -0400

    Refactor dist_train.sh

--- dist_train.sh
+++ dist_train.sh
@@ -4,19 +4,11 @@ NOTE: replace torchrun with torch.distributed.launch if you use older version of
 '''
 
 
-## bisenetv2 mastr
+## bisenetv2 mastr (THE ENVGO CONFIGURATION)
 export CUDA_VISIBLE_DEVICES=0
 cfg_file=configs/bisenetv2_mastr.py
 NGPUS=1
 torchrun --nproc_per_node=$NGPUS tools/train_amp.py --config $cfg_file
-
-
-## bisenetv1 cityscapes
-export CUDA_VISIBLE_DEVICES=0,1
-cfg_file=configs/bisenetv1_city.py
-NGPUS=2
-torchrun --nproc_per_node=$NGPUS tools/train_amp.py --config $cfg_file
-
 
 ## bisenetv2 cityscapes
 export CUDA_VISIBLE_DEVICES=0,1
@@ -25,23 +17,9 @@ NGPUS=2
 torchrun --nproc_per_node=$NGPUS tools/train_amp.py --config $cfg_file
 
 
-## bisenetv1 cocostuff
-export CUDA_VISIBLE_DEVICES=0,1,2,3
-cfg_file=configs/bisenetv1_coco.py
-NGPUS=4
-torchrun --nproc_per_node=$NGPUS tools/train_amp.py --config $cfg_file
-
-
 ## bisenetv2 cocostuff
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 cfg_file=configs/bisenetv2_coco.py
-NGPUS=8
-torchrun --nproc_per_node=$NGPUS tools/train_amp.py --config $cfg_file
-
-
-## bisenetv1 ade20k
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
-cfg_file=configs/bisenetv1_ade20k.py
 NGPUS=8
 torchrun --nproc_per_node=$NGPUS tools/train_amp.py --config $cfg_file
 
